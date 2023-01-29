0f1f322b57b7655d99040860e21c8d75f3a90189
vonrimS <mr.mytab@gmail.com>
2023-01-30 00:33:11 +0100

    setup for mailer service

---
+++ mailer/src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+// const axios = require('axios')
+// const { connectDb } = require('./helpers/db');
+const { port, host, apiUrl } = require('./configuration/index');
+
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`service AUTH was started on port: ${port}`);
+    });
+};
+
+app.get('/test', (req, res) => {
+    res.send('Our MAILER server is working');
+});
+
+app.get('/send', (req, res) => {
+    console.log('...email sent')
+    res.json({ message: 'email sent '})
+})
+
+
+startServer();
+// communication test to fetch data from api service
+// app.get('/test-with-api-data', (req, res) => {
+//     axios.get(apiUrl + '/test-api-data').then(response=>{
+//         res.json({
+//             "done": true,
+//             "response": response.data,
+//             "testdata": response.data.testdata
+//         })
+//     })
+// })
+
+// test request between services
+// app.get('/api/currentUser', (req,res)=> {
+//     res.json({
+//         id: '1234',
+//         email: 'foo@gmail.com'
+//     })
+// })
+
+// connectDb()
+//     .on("error", console.log)
+//     .on("disconnected", connectDb)
+//     .once("open", startServer);
