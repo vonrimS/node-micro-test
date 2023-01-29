b4e70b38d9feed912e2cc2258f5a4884673246b4
vonrimS <mr.mytab@gmail.com>
2023-01-29 23:05:24 +0100

    prod dev separation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to 0654654000.
+          Edit <code>src/App.js</code> and save to hello sdfsd01.
         </p>
         <a
           className="App-link"
